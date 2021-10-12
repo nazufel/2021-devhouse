7dfa8cc68262c16eec4d7b61ad97003359c14d12
nazufel <38038720+nazufel@users.noreply.github.com>
2021-10-11 22:43:12 -0400

    working on the ansible bit

--- terraform/app_servers.tf
+++ terraform/app_servers.tf
@@ -25,8 +25,8 @@ resource "google_service_account" "devhouse" {
   display_name = "Devhouse Service Account"
 }
 
-resource "google_compute_instance" "app_server_01" {
-  name         = "devhouse-app-server"
+resource "google_compute_instance" "app_01" {
+  name         = "app-01"
   machine_type = "e2-medium"
   zone         = "${var.DEVHOUSE_2021_GCP_REGION}-a"
 
@@ -53,6 +53,7 @@ resource "google_compute_instance" "app_server_01" {
 
   metadata = {
     environment = "devhouse"
+    ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCY1fhNvh29dI1qGAmZrRNr8W2DmNtqOQYlVvssduAl1kfuhLQ5h2eI9AtIiNtQ4ej9rb732cUV5pZph7TwkFgMISKy0lcI4IZdLUBnfS7o7IAB0jInCPYtTrGPykDno937q7dzjDFEdV8MIrmAGPLgz+aGesVHxU5oUPlNGaMrGf9rBkt9qz31o6VEaH7DAjR59aWXox4oS/2Db8HveaSAGQ3HJmAKMQ0GutI8vpLO7mNA2CAtRASgdG4oyvSaCTDRzoJJFVQC/nheCYcwmzLKtTs24saxhikDv89Dt7NxmE/juh6YtpKhuaaewx8wY7JLtZlrYAkq86Hur4Hx995hUW0sN3oezWnmdnVe+v59XIKAjvOuALTChxvTDlqMh+5ss2BNQgEyzLx+kGLG98iHOiOa2Lc9mtyyKFruy1fjLZcwZkfsj6x/PYgGux1FeZN3zITcN6OMNQngaSLBfzmSJv1wNNBlNbQGspDruPcgOY9zfMviJjDL6NpgxcHGVks= rross@gloomwalker"
   }
 
   metadata_startup_script = local.metadata_startup_script
@@ -75,17 +76,17 @@ resource "google_compute_instance" "app_server_01" {
   ]
 }
 
-resource "google_dns_record_set" "app_server_01" {
-  name    = format("${google_compute_instance.app_server_01.name}.%s", google_dns_managed_zone.devhouse_private_zone.dns_name)
+resource "google_dns_record_set" "app_01" {
+  name    = format("${google_compute_instance.app_01.name}.%s", google_dns_managed_zone.devhouse_private_zone.dns_name)
   project = var.DEVHOUSE_2021_GCP_PROJECT
   type    = "A"
   # 30 second TTL
   ttl          = 30
   managed_zone = google_dns_managed_zone.devhouse_private_zone.name
-  rrdatas      = [google_compute_instance.app_server_01.network_interface.0.network_ip]
+  rrdatas      = [google_compute_instance.app_01.network_interface.0.network_ip]
 
   depends_on = [
     google_dns_managed_zone.devhouse_private_zone,
-    google_compute_instance.app_server_01
+    google_compute_instance.app_01
   ]
 }
